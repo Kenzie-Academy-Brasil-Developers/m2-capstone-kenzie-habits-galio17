0f79c1d095be4430a7690af7a69c06f790736c1c
wendelbueres <wendelbueres@gmail.com>
2022-07-12 18:24:26 -0300

    Fix: adicionada requisição updateUser

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,29 +1,24 @@
-class Login {
-    static base_url = 'https://habits-kenzie.herokuapp.com/api/userLogin'
-    static statusRequisicao
-
-    static async logar(dataLogin){ 
-        return await fetch(this.base_url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(dataLogin)
-        })
-        .then(res => {
-            this.statusRequisicao = res.status
-            return res.json()})
-        .then((response) => {
-            localStorage.setItem('@kenzie-habits:user_name', response.response.usr_name)
-            localStorage.setItem('@kenzie-habits:user_email', response.response.usr_email)
-            localStorage.setItem('@kenzie-habits:user_img', response.response.usr_image)
-            localStorage.setItem('@kenzie-habits:token', response.token)
-            return response
-        })
-
-        .catch(err => console.log(err))
+class User {
+    static base_url = 'https://habits-kenzie.herokuapp.com/api/user/profile'
+    static headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('@kenzie-habits:token')}`
     }
+        static async updateProfile(dataUpdate){ 
+            return await fetch(this.base_url, {
+                method: "PATCH",
+                headers: this.headers,
+                body: JSON.stringify(dataUpdate)
+            })
+            .then(res => res.json())
+            .then((response) => {
+                console.log(response)
+                return response
+            })
+
+            .catch(err => console.log(err))
+        }
 
 }
 
-export { Login }
+export { User }
