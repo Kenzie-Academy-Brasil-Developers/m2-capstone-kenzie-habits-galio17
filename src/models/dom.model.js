94834ba87e4b93eaddb74a22178fb12e229adce7
Giuseppe Dias <giuseppe.dias17@gmail.com>
2022-07-14 18:16:58 -0300

    del: exclusão de método de criar formularios

--- src/models/dom.model.js
+++ src/models/dom.model.js
@@ -50,54 +50,6 @@ export default class Dom {
         //modal.style.display = "flex"
         //})
     }
-    static createForm(textoBotao, inputDados, temCategorias, temBotaoExcluir) {
-        const form = document.createElement('form');
-        const botaoEnviar = document.createElement('button');
-
-        botaoEnviar.type = 'submit';
-        botaoEnviar.innerText = textoBotao;
-
-        inputDados.forEach(dado => {
-            const novoLabel = document.createElement('label');
-            const novoInput = document.createElement('input');
-
-            novoLabel.innerText = dado.label;
-            novoLabel.htmlFor = dado.name;
-
-            novoInput.type = dado.type;
-            novoInput.name = dado.name;
-            novoInput.id = dado.name;
-
-            form.append(novoLabel, novoInput);
-        });
-
-        if(temCategorias) {
-            const categoriaSelect = document.createElement('select');
-
-            categoriaSelect.insertAdjacentHTML('afterbegin', `
-                <option value="casa">Casa</option>
-                <option value="estudo">Estudo</option>
-                <option value="lazer">Lazer</option>
-                <option value="trabalho">Trabalho</option>
-                <option value="saude">Saúde</option>
-            `);
-
-            form.append(categoriaSelect);
-        }
-
-        if(temBotaoExcluir) {
-            const botaoExcluir = document.createElement('button');
-
-            botaoExcluir.innerText = 'Excluir';
-
-            form.append(botaoExcluir)
-        }
-
-        form.append(botaoEnviar);
-
-        return form;
-    }
-
 
     static modelSucessLogin(sucesso){
 
